66e6c95085e3aa9d3ca4d3ef6dc7982710c9d179
Gabriel Nordeborn <gabbe.nord@gmail.com>
2021-12-13 20:47:55 +0100

    fix Discord link

--- rescript-relay-documentation/docusaurus.config.js
+++ rescript-relay-documentation/docusaurus.config.js
@@ -51,7 +51,7 @@ module.exports = {
           items: [
             {
               label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
+              href: "https://discord.gg/wzj4EN8XDc",
             },
           ],
         },
