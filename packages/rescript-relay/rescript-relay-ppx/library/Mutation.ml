624b1bf5acee3465138a0d2648c5595824fe0929
Gabriel Nordeborn <gabbe.nord@gmail.com>
2023-05-31 20:02:49 +0200

    unwind mutation

--- packages/rescript-relay/rescript-relay-ppx/library/Mutation.ml
+++ packages/rescript-relay/rescript-relay-ppx/library/Mutation.ml
@@ -13,58 +13,6 @@ let make ~loc ~moduleName =
          [%stri module Operation = [%m moduleIdentFromGeneratedModule []]];
          [%stri module Types = [%m moduleIdentFromGeneratedModule ["Types"]]];
          [%stri
-           type updaterFn =
-             RescriptRelay.RecordSourceSelectorProxy.t ->
-             [%t typeFromGeneratedModule ["Types"; "response"]] ->
-             unit
-           [@@live]];
-         [%stri
-           type optimisticUpdaterFn =
-             RescriptRelay.RecordSourceSelectorProxy.t -> unit
-           [@@live]];
-         [%stri
-           type useMutationConfig = {
-             onError: (RescriptRelay.mutationError -> unit) option;
-             onCompleted:
-               ([%t typeFromGeneratedModule ["Types"; "response"]] ->
-               RescriptRelay.mutationError array option ->
-               unit)
-               option;
-             onUnsubscribe: (unit -> unit) option;
-             optimisticResponse:
-               [%t typeFromGeneratedModule ["Types"; "rawResponse"]] option;
-             optimisticUpdater: optimisticUpdaterFn option;
-             updater: updaterFn option;
-             variables: [%t typeFromGeneratedModule ["Types"; "variables"]];
-             uploadables: RescriptRelay.uploadables option;
-           }
-           [@@live]];
-         [%stri
-           type useMutationConfigRaw = {
-             onError: (RescriptRelay.mutationError -> unit) option;
-             onCompleted:
-               ([%t typeFromGeneratedModule ["Types"; "response"]] ->
-               RescriptRelay.mutationError array Js.Nullable.t ->
-               unit)
-               option;
-             onUnsubscribe: (unit -> unit) option;
-             optimisticResponse:
-               [%t typeFromGeneratedModule ["Types"; "rawResponse"]] option;
-             optimisticUpdater: optimisticUpdaterFn option;
-             updater: updaterFn option;
-             variables: [%t typeFromGeneratedModule ["Types"; "variables"]];
-             uploadables: RescriptRelay.uploadables option;
-           }
-           [@@live]];
-         [%stri
-           [%%private
-           external internal_useMutation :
-             [%t typeFromGeneratedModule ["relayOperationNode"]]
-             RescriptRelay.mutationNode ->
-             (useMutationConfigRaw -> RescriptRelay.Disposable.t) * bool
-             = "useMutation"
-             [@@module "react-relay"] [@@live]]];
-         [%stri
            let convertVariables :
                [%t typeFromGeneratedModule ["Types"; "variables"]] ->
                [%t typeFromGeneratedModule ["Types"; "variables"]] =
@@ -85,91 +33,8 @@ let make ~loc ~moduleName =
                ~convertResponse ~convertWrapRawResponse
                ~node:[%e valFromGeneratedModule ["node"]]];
          [%stri
-           let use :
-               unit ->
-               (?onError:(RescriptRelay.mutationError -> unit) ->
-               ?onCompleted:
-                 ([%t typeFromGeneratedModule ["Types"; "response"]] ->
-                 RescriptRelay.mutationError array option ->
-                 unit) ->
-               ?onUnsubscribe:(unit -> unit) ->
-               ?optimisticResponse:
-                 [%t typeFromGeneratedModule ["Types"; "rawResponse"]] ->
-               ?optimisticUpdater:optimisticUpdaterFn ->
-               ?updater:
-                 (RescriptRelay.RecordSourceSelectorProxy.t ->
-                 [%t typeFromGeneratedModule ["Types"; "response"]] ->
-                 unit) ->
-               variables:[%t typeFromGeneratedModule ["Types"; "variables"]] ->
-               ?uploadables:RescriptRelay.uploadables ->
-               unit ->
-               RescriptRelay.Disposable.t)
-               * bool =
-            fun () ->
-             let mutate, mutating =
-               internal_useMutation [%e valFromGeneratedModule ["node"]]
-             in
-             ( React.useMemo1
-                 (fun () ?onError ?onCompleted ?onUnsubscribe
-                      ?optimisticResponse ?optimisticUpdater ?updater ~variables
-                      ?uploadables () ->
-                   mutate
-                     {
-                       onError;
-                       onCompleted =
-                         (match onCompleted with
-                         | Some fn ->
-                           Some
-                             (fun r errors ->
-                               fn
-                                 (r
-                                 |. [%e
-                                      valFromGeneratedModule
-                                        ["Internal"; "convertResponse"]])
-                                 (Js.Nullable.toOption errors))
-                         | None -> None);
-                       optimisticResponse =
-                         (match optimisticResponse with
-                         | None -> None
-                         | Some r ->
-                           Some
-                             (r
-                             |. [%e
-                                  valFromGeneratedModule
-                                    ["Internal"; "convertWrapRawResponse"]]));
-                       onUnsubscribe;
-                       variables =
-                         variables
-                         |. [%e
-                              valFromGeneratedModule
-                                ["Internal"; "convertVariables"]];
-                       optimisticUpdater;
-                       uploadables;
-                       updater =
-                         (match updater with
-                         | None -> None
-                         | Some updater ->
-                           Some
-                             (fun store r ->
-                               updater store
-                                 (r
-                                 |. [%e
-                                      valFromGeneratedModule
-                                        ["Internal"; "convertResponse"]])));
-                     })
-                 [|mutate|],
-               mutating )
-            [@@ocaml.doc
-              "React hook for commiting this mutation.\n\n\
-               ### Optimistic updates\n\
-               Remember to annotate your mutation with `@raw_response_type` if \
-               you want to do optimistic updates. That'll make Relay emit the \
-               required type information for covering everything needed when \
-               doing optimistic updates."]
-            [@@live]];
+           let use =
+             RescriptRelay_Migrate.Mutation.useMutation ~convertVariables
+               ~convertResponse ~convertWrapRawResponse
+               ~node:[%e valFromGeneratedModule ["node"]]];
        ])
-  [@@ocaml.doc
-    "\n\
-    \ * Check out the comments for makeFragment, this does the same thing but \
-     for mutations.\n\
-    \ "]
--- packages/rescript-relay/src/RescriptRelay_Migrate.res
+++ packages/rescript-relay/src/RescriptRelay_Migrate.res
@@ -25,11 +25,28 @@ module Mutation = {
     uploadables?: uploadables,
   }
 
+  type useMutationConfigRaw<'m, 'variables, 'response, 'rawResponse> = {
+    onError?: Js.Nullable.t<mutationError> => unit,
+    onCompleted?: ('response, Js.Nullable.t<array<mutationError>>) => unit,
+    onUnsubscribe?: unit => unit,
+    optimisticResponse?: 'rawResponse,
+    optimisticUpdater?: optimisticUpdaterFn,
+    updater?: updaterFn<'response>,
+    variables: 'variables,
+    uploadables?: uploadables,
+  }
+
   @module("relay-runtime")
   external commitMutation_: (
     Environment.t,
     commitMutationConfigRaw<'m, 'variables, 'response, 'rawResponse>,
   ) => Disposable.t = "commitMutation"
+
+  @module("react-relay")
+  external useMutation_: 'm => (
+    useMutationConfigRaw<'m, 'variables, 'response, 'rawResponse> => Disposable.t,
+    bool,
+  ) = "useMutation"
 
   let commitMutation = (
     ~convertVariables: 'variables => 'variables,
@@ -78,6 +95,55 @@ module Mutation = {
           variables: variables->convertVariables,
         },
       )
+    }
+  }
+
+  let useMutation = (
+    ~convertVariables: 'variables => 'variables,
+    ~node: 'm,
+    ~convertResponse: 'response => 'response,
+    ~convertWrapRawResponse: 'rawResponse => 'rawResponse,
+  ) => {
+    /**React hook for commiting this mutation.\n\n\
+               ### Optimistic updates\n\
+               Remember to annotate your mutation with `@raw_response_type` if \
+               you want to do optimistic updates. That'll make Relay emit the \
+               required type information for covering everything needed when \
+               doing optimistic updates.*/
+    () => {
+      let (mutate, mutating) = useMutation_(node)
+      (React.useMemo1(() => {
+          (
+            ~variables: 'variables,
+            ~optimisticUpdater=?,
+            ~optimisticResponse: option<'rawResponse>=?,
+            ~updater=?,
+            ~onCompleted=?,
+            ~onError=?,
+            ~uploadables=?,
+            (),
+          ) => {
+            mutate({
+              onCompleted: ?switch onCompleted {
+              | Some(cb) => Some((res, err) => cb(res->convertResponse, err))
+              | None => None
+              },
+              ?onError,
+              optimisticResponse: ?switch optimisticResponse {
+              | Some(optimisticResponse) => Some(optimisticResponse->convertWrapRawResponse)
+              | None => None
+              },
+              ?optimisticUpdater,
+              updater: ?switch updater {
+              | Some(updater) =>
+                Some((store, response) => updater(store, response->convertResponse))
+              | None => None
+              },
+              ?uploadables,
+              variables: variables->convertVariables,
+            })
+          }
+        }, [mutate]), mutating)
     }
   }
 }
