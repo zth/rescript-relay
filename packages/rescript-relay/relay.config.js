79e8f455a2608f431bee509aa58d7ee7c845d90e
Gabriel Nordeborn <gabbe.nord@gmail.com>
2026-02-16 13:12:30 +0100

    pull latest compiler to actually wire up autoExhaustive

--- packages/rescript-relay/relay.config.js
+++ packages/rescript-relay/relay.config.js
@@ -7,6 +7,7 @@ module.exports = {
     : "./__tests__/__generated__",
   src: PERSISTING ? "./__tests_preloaded__" : "./__tests__",
   language: "rescript",
+  autoExhaustiveTypes: [],
   customScalarTypes: {
     Datetime: "TestsUtils.Datetime",
     IntString: "TestsUtils.IntString",
